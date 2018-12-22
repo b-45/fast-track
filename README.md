a84d68612c4b267b795a56f965629a491df4b8c4
b-45 <bernard11ade@gmail.com>
2018-12-22 12:06:39 -0500

    added color palette

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-##Static mock up of a order management app in Vue.js
+## Static mock up of a order management app in Vue.js
+
 ![title](https://github.com/b-45/fast-track/blob/master/src/assets/title.png)
 ![image](https://github.com/b-45/fast-track/blob/master/src/assets/frames.png)
 
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -2,6 +2,19 @@ import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
 import 'vuetify/src/stylus/app.styl'
 
+
 Vue.use(Vuetify, {
   iconfont: 'md',
+  theme: {
+    primary: '#3490DC',
+    primary2: '#B8C2CC',
+    success: '#38C172',
+    info: '#6574CD',
+    error: '#E3342F',
+    sidebar: '#303030',
+    primary3: '#9e9e9e',
+    myyellow: '#ffc247',
+    mytext: '#495057',
+    myblue: '#1c7ed6'
+  }
 })
